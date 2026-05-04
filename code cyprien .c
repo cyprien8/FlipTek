9d59470f62eb541ea2f3526ac0ca1a0faee5acbf
cyprien8 <bayardcyp@gmail.com>
2026-05-04 09:33:53 +0200

    Update code cyprien .c

--- code cyprien .c
+++ code cyprien .c
@@ -1,32 +1,32 @@
- void vider_main(Joueur *j){
+  void vider_main(Joueur *j){ // fonction pour enlever toutes les cartes d'un joueur en fin de manche 
      j->taille_main=0;
  }
  
- void ajouter_carte(Joueur *j, Carte c){
+ void ajouter_carte(Joueur *j, Carte c){ // fonction pour ajouter une carte a la main d'un joueur ( pioche )
      if (j->taille_main<7){
          j->main[j->taille_main]=c;
          j->taille_main++;
      }
  }
  
- void affiche_carte( Carte c){
+ void affiche_carte( Carte c){ // effet visuel pour distinguer les cartes bonus des autres 
      
      if(c.est_bonus==1){
-        printf("\033[33m");
+        printf("\033[33m"); // mise en couleur jaune 
         printf("-------\n");
         printf("|     |\n");
         printf("|  %s  |\n", c.label);
         printf("|     |\n");
         printf("-------\n");
-        printf("\033[0m");
+        printf("\033[0m"); // on revient à la couleur par défaut 
      }
      else{
-        printf("\033[37m");
+        printf("\033[37m"); // mise en couleur blanc 
         printf("-------\n");
         printf("|     |\n");
         printf("|  %s  |\n", c.label);
         printf("|     |\n");
         printf("-------\n");
-        printf("\033[0m");
+        printf("\033[0m"); // on revient à la couleur par défaut 
      }
  }
