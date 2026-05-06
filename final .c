f7455397974416a5f3bdfba398c43ea3e66d8b3c
TitouanLeMirronet <titouan07lm@gmail.com>
2026-05-06 09:53:55 +0200

    Update final .c

--- final .c
+++ final .c
@@ -1,1 +1,23 @@
+int main(){
+  int nbjoueurs;
+  do{
+  printf("Quel est le nombre de joueurs ?");
+  scanf("%d",&nbjoueurs);
+  }while(nbjoueurs<=0); // Demande du nombre de joueurs
+
+  Joueur *listejoueur=malloc(sizeof(Joueur)*nbjoueurs);
+  if(listejoueur==NULL){
+    printf("erreur d'allocation");
+    exit(1); // création du tableau joueur 
+  }
+  for(int i=0; i<nbjoueurs; i++){
+    printf("Quel est le pseudo du joueur %d",i+1);
+    scanf("%s",listejoueur[i].nom); // remplissage des pseudos dans le tableau
+    listejoueur[i].score=0;
+  }
+
+
+  free(listejoueur);
+  return 0;
+}
 
