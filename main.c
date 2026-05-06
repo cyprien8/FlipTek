4dd7906a4a0bd90cbbdfdff98645815f66c6ed60
TitouanLeMirronet <titouan07lm@gmail.com>
2026-05-06 11:18:42 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include "final.h"
 
-int tour(Joueur* jeu_joueur, int nbjoueurs, int* pioche, ){
+int tour(Joueur* jeu_joueur, int nbjoueurs, ){
   if(nbjoueurs==0){
     return 0;
   }
-  ajouter_carte(jeu_joueur[0], Carte c)
+  ajouter_carte(jeu_joueur[0],)
   //Appel de la fonction piocher du premier joueur(tab[0])
 
-  return tour(tab+1,nbjoueurs-1);
+  return tour(jeu_joueur+1,nbjoueurs-1);
     
 }
 
@@ -21,16 +21,19 @@ int main(){
   }while(nbjoueurs<=0); // Demande du nombre de joueurs
 
   Joueur *listejoueur=malloc(sizeof(Joueur)*nbjoueurs);
+  
   if(listejoueur==NULL){
     printf("erreur d'allocation");
     exit(1); // création du tableau joueur 
   }
+  
   for(int i=0; i<nbjoueurs; i++){
     printf("Quel est le pseudo du joueur %d",i+1);
     scanf("%s",listejoueur[i].nom); // remplissage des pseudos dans le tableau
     listejoueur[i].score_total=0;
   }
-  pioche_base(p); //appel de la fonction pour créer la pioche
+  
+  pioche_base(p); //appel de la fonction pour initialise la pioche
   melange_pioche(pioche); //appel de la fonction pour mélanger la pioche
 
 
