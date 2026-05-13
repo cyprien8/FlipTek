b65a07989d5098a4eb78bc2e247244a217a1d240
TitouanLeMirronet <titouan07lm@gmail.com>
2026-05-14 00:08:40 +0200

    Update titouan.c

--- titouan.c
+++ titouan.c
@@ -37,3 +37,52 @@ int recherche_dichotomique_main(Carte* tab, int debut, int fin, Carte carte){ //
     return recherche_dichotomique_main(tab+1,debut,millieu-1,carte);
   }
 }
+
+
+
+void jouer_manche(Joueur* listejoueur, int nbjoueurs, Pioche ppioche){
+int manche_en_cours=1;
+while(manche_en_cours==1){
+  //for(int i=0;i<nbjoueurs;i++){ initialisation des parametre, on parcourt les joueurs
+    vider_main(listejoueur);
+    listejoueur[0].est_actif=1;
+    listejoueur[0].score_manche=0;
+
+    //debut de la manche 
+    char jeu;
+    int n;
+    Carte carte_pioche;
+    if(listejoueur[0].est_actif==1){ //est ce que le joueur est toujours dans le jeu
+      printf("Ecrire 'A' si tu souhaites t'arreter et 'P' si tu souhaites piocher");
+      scanf("%c",&jeu);
+      if(jeu=='A'){
+        listejoueur[0].est_actif=0; //si il s'arrete il devient inactif
+      }
+      else if(jeu=='P'){
+        carte_pioche=carte_dessus(ppioche);//sinon on retourne une carte de la pioche
+        affiche_carte(carte_pioche); //affichage de la carte
+        n=recherche_dichotomique_main(listejoueur[0].main,0,nbjoueurs,carte_pioche); 
+        if(n==1){
+          printf("La carte pioché est deja dans la main");
+          listejoueur[0].est_actif=0; //devient inactif
+          listejoueur[0].score_manche=0; //il gagne aucun point durant cette manche
+        }
+        else{
+          ajouter_carte(listejoueur, carte_pioche);
+          tri_main(listejoueur[0].main,listejoueur[0].taille_main);
+        }
+      }
+    }
+    manche_en_cours=arret_manche(listejoueur,nbjoueurs);
+      if(manche_en_cours==0){ 
+        printf("Arret de la manche car un joueur a atteint le maximum de carte");
+      }
+      if(manche_en_cours==2){
+        printf("Plus aucun joueur n'est actif");
+      }
+      if(manche_en_cours==1){
+        printf("Continuation de la manche");
+      }
+  }
+  listejoueur[0].score_manche=somme_main(listejoueur);
+}
