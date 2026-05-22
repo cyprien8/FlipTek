657e3f84b49487a44888b711ad987a8163638593
Math_gb <mathisgastonbreton@gmail.com>
2026-05-22 15:10:16 +0200

    Update main_final.c

--- main_final.c
+++ main_final.c
@@ -30,7 +30,7 @@ int main() {
 do{
   printf("Quel est le nombre de joueurs ?");
   scanf("%d",&nbjoueurs);
-  if (nbjoueurs <= 0) {
+  if (nbjoueurs <= 0 && nbjoueurs>=86) {
         printf("Nombre de joueurs invalide.Veuillez mettre un nombre superieur à 0.\n");
     }
   }while(nbjoueurs<=0 && nbjoueurs>=86); 
